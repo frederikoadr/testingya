7a9f3b2e9b93cd86ccc3ccdda93c233777cfa71f
Frederiko Adrian <frederiko.adrian@gmail.com>
2023-08-30 18:14:57 +0700

    init commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def generate(numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [list(map(lambda x, y: x+y, res[-1] + [0], [0] + res[-1]))]
+        return res[:numRows]
+print(Solution.generate(2))
